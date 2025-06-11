91d01b328297950c3c7a4d4770b176a0d8921230
divyanshsharma7 <divyanshsharma01092005@gmail.com>
2025-06-11 08:24:05 +0530

    added

--- Linked list/Single_linkedlist/insertion_element_start_singly.py
+++ Linked list/Single_linkedlist/insertion_element_start_singly.py
@@ -46,3 +46,48 @@ obj.prepend(5)
 print(obj)
     
     
+    
+    
+# class Node:
+#     def __init__(self,value):
+#         self.value=value
+#         self.next=None
+# class Linkedlist:
+#     def __init__(self):
+#         self.head=None
+#         self.tail=None
+#         self.length=0
+#     def append(self,value):
+#         new_node=Node(value)
+#         if self.head is None:
+#             self.head=new_node
+#             self.tail=new_node
+#         else:
+#             self.tail.next=new_node
+#             self.tail=new_node
+#         self.length+=1
+#     def __str__(self):    
+#         temp_node = self.head
+#         result=" "
+#         while temp_node is not None:
+#             result+=str(temp_node.value)
+#             if temp_node.next is not None:
+#                 result += " ---> "
+#             temp_node=temp_node.next
+#         return result               
+#     def prepend(self,value):
+#         new_node=Node(value)
+#         if self.head is None:
+#             self.head=new_node
+#             self.tail=new_node
+#         else:
+#             new_node.next=self.head
+#             self.head=new_node
+#         self.length+=1
+        
+# obj=Linkedlist()
+# obj.append(10)
+# obj.append(20)
+# obj.append(30)
+# print(obj)
+# obj.prepend(5)
