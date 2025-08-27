5fe5e1ad1aabec544c5fbc1532133c7071a80b38
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-08-27 21:14:21 +0530

    added

---
+++ Linked list/Doubly linked list/append.py
@@ -0,0 +1,36 @@
+
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+        self.prev=None
+    def __str__(self):
+        return str(self.value)
+
+class DoublyLinkedlist:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            new_node.prev=self.tail
+            self.tail=new_node
+        self.length+=1
+obj=DoublyLinkedlist()
+obj.append(10)
+obj.append(20)
+obj.append(30)
+print(obj.head.next)
+
+
+            
+            
+        
+        
---
+++ Linked list/Doubly linked list/creation.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value):
+        self.value=value
+        self.next=None
+        self.prev=None
+
+    def __str__(self):
+        return f"{self.prev} <- {self.value} -> {self.next}"
+    
+
+    
+obj=Node(10)
+print(obj)
