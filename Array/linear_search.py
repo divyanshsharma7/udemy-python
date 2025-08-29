ac2b74185e1e0b104cbd98ff0da5dac5103a1ddb
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-08-29 14:15:30 +0530

    added

--- Array/linear_search.py
+++ Array/linear_search.py
@@ -1,9 +1,9 @@
+import numpy as np
 import array
-arr=array.array("i", [1,34,23,56,78,89])
+arr=array.array("i",[23,12,34,56,4,3])
 def linear_search(arr, target):
     for index in range(len(arr)):
-        if arr[index]==target:
+        if arr==target:
             return index
     return -1
-print(linear_search(arr, 34))  # it will return the index of the element 
-print(linear_search(arr, 100)) # -1 means not found
+print(linear_search(arr,23))
