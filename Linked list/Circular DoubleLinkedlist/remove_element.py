07757e893b54fa95845efe009f96023d95b5402a
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-02 22:07:23 +0530

    added

---
+++ Linked list/Circular DoubleLinkedlist/remove_element.py
@@ -0,0 +1,68 @@
+
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+        self.prev=None
+    def __str__(self):
+        return str(self.value)
+    
+class CircularDoublylinkedlist:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def __str__(self):
+        temp_node=self.head
+        result= ''
+        while temp_node is not None:
+            result +=str(temp_node.value)
+            temp_node=temp_node.next
+            if temp_node==self.head:
+                break
+            result +=' -> '
+        return result
+    
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.head.prev=new_node
+            new_node.prev=self.tail
+            self.tail=new_node
+        self.length+=1
+
+    def get(self, index):
+        current_node=None
+        if index < self.length//2:
+            current_node=self.head
+            for i in range(index):
+                current_node=current_node.next
+        else:
+            current_node=self.tail
+            for i in range(self.length -1, index, -1):
+                current_node=current_node.prev
+
+        return current_node
+    
+    def remove(self, index):
+        pop_node=self.get(index)
+        pop_node.next.prev=pop_node.prev
+        pop_node.prev.next=pop_node.next
+        pop_node.next=None
+        pop_node.prev=None
+        self.length-=1
+        return pop_node
+        
+
+obj=CircularDoublylinkedlist()
+obj.append(10)
+obj.append(20)
+obj.append(30)
+print(obj)
+obj.remove(1)
+print(obj)
