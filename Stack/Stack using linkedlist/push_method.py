1996476da7e9e6dc1971613226be395d8fc1afb2
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-03 21:23:00 +0530

    added

---
+++ Stack/Stack using linkedlist/push_method.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+        
+
+class Stack:
+
+    def __init__(self):
+        self.top=None
+        self.length=0
+
+    def push(self,value):
+        new_node=Node(value)
+        new_node.next=self.top
+        self.top=new_node
+        self.length+=1
+
+obj=Stack()
+obj.push(10)
+obj.push(20)
+obj.push(30)
+print(obj.top.value)
