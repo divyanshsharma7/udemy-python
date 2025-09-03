6a9a9243027ab0161322f97e7fbf20f05780db6a
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-03 20:20:51 +0530

    added

--- Stack/Stack using list/push_operation.py
+++ Stack/Stack using list/push_operation.py
@@ -2,6 +2,16 @@ class Stack:
     def __init__(self):
         self.item=[]
 
+    def is_empty(self):
+        return len(self.item)==0
+
+    def __str__(self):
+        if self.is_empty():
+            return "Stack is empty"
+        values=[str(x) for x in reversed(self.item)]
+        return '\n'.join(values)
+
+
     def push(self,element):
         self.item.append(element)
 
@@ -9,4 +19,4 @@ obj=Stack()
 obj.push(10)
 obj.push(20)
 obj.push(30)
-print(obj.item)
+print(obj)
