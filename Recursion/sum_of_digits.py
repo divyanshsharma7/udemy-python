37281f7b2a22477faf4cde5e14583837a0dd3414
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-08 12:52:43 +0530

    added

---
+++ Recursion/sum_of_digits.py
@@ -0,0 +1,8 @@
+def sum_of_digits(n):
+    assert n>=0 and int(n)==n, "The number has to be only positive number"
+    if n ==0:
+        return 0
+    else:
+        return int(n%10) + sum_of_digits(int(n/10))
+
+print(sum_of_digits(15))
