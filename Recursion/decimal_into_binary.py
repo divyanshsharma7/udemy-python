19f710072815a655b3211608f4d5482f1429ff67
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-10 11:28:33 +0530

    added

---
+++ Recursion/decimal_into_binary.py
@@ -0,0 +1,8 @@
+def decimal_to_binary(n):
+    assert int(n) ==n,"THe parameter must be an integer only"
+    if n==0:
+        return 0
+    else:
+        return n%2 +10 *decimal_to_binary(int(n/2))
+
+print(decimal_to_binary(10))
