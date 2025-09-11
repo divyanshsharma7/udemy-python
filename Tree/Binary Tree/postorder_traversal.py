bafd4fc966995d9b5927b8ca2ffb9aa2648fe9b4
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-11 12:29:34 +0530

    added

---
+++ Tree/Binary Tree/postorder_traversal.py
@@ -0,0 +1,35 @@
+class Treenode:
+    def __init__(self,data):
+        self.data=data
+        self.leftChild=None
+        self.rightChild=None
+
+obj=Treenode("Drinks")
+leftChild=Treenode("Hot")
+rightChild=Treenode("Cold")
+obj.leftChild=leftChild
+obj.rightChild=rightChild
+
+def preorder(rootnode):
+    if not rootnode:
+        return
+    print(rootnode.data)
+    preorder(rootnode.leftChild)
+    preorder(rootnode.rightChild)
+
+def inorder_traversal(rootnode):
+    if not rootnode:
+        return
+    inorder_traversal(rootnode.leftChild)
+    print(rootnode.data)
+    inorder_traversal(rootnode.rightChild)
+
+def postorder_traversal(rootnode):
+    if not rootnode:
+        return
+    postorder_traversal(rootnode.leftChild)
+    postorder_traversal(rootnode.rightChild)
+    print(rootnode.data)
+
+postorder_traversal(obj)
+
