5413006319a77eaa7f40aeca5239c072bca0c662
Divyansh sharma <divyanshsharma01092005@gmail.com>
2025-09-11 11:57:48 +0530

    added

--- Tree/creation.py
+++ Tree/creation.py
@@ -27,3 +27,37 @@ hot.addchild(tea)
 hot.addchild(coffee)
 
 print(tree)
+
+
+
+# class Treenode:
+#     def __init__(self,data,children=[]):
+#         self.data=data
+#         self.children=children
+
+#     def __str__(self,level=0):
+#         ret =" " * level + str(self.data) + "\n"
+#         for child in self.children:
+#             ret += child.__str__(level+1)
+#         return ret
+    
+#     def addChild(self,Treenode):
+#         self.children.append(Treenode)
+
+# tree=Treenode("Drinks",[])
+# cold=Treenode("Cold",[])
+# hot=Treenode("Hot",[])
+
+# tree.addChild(cold)
+# tree.addChild(hot)
+# cola=Treenode("Cola",[])
+# fanta=Treenode("Fanta",[])
+# tea=Treenode("Tea",[])
+# coffee=Treenode("Coffee",[])
+# cold.addChild(cola)
+# cold.addChild(fanta)
+# hot.addChild(tea)
+# hot.addChild(coffee)
+
+# print(tree)
+
